cfa44aa9de890cc6abf760337590533c4398b0fe
yassin-esprit <yassin.jaouani@esprit.tn>
2024-01-26 21:26:03 +0100

    js

--- atelier_js_1/index(dom6).js
+++ atelier_js_1/index(dom6).js
@@ -1,4 +1,12 @@
 function calculeScore() {
-  if (condition) {
+  var score = document.getElementsByName("choix").values;
+  var choix = document.querySelectorAll('input[name="choix"]:checked');
+  var nbchoix = choix.length;
+  if (nbchoix > 3 || nbchoix < 3) {
+    alert("choisir 3 choix !!");
+  } else {
+    if (score > 15) {
+      alert("Vous etes deja un joueur expert ");
+    } else alert("Vous etes encore un joueur debutant");
   }
 }
