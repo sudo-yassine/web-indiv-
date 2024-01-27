5ca142cf59282003d462b1102d9d2b1b0a0e3311
yassin-esprit <yassin.jaouani@esprit.tn>
2024-01-27 14:04:48 +0100

    atelier_js_1

--- atelier_js_1/index(dom6).js
+++ atelier_js_1/index(dom6).js
@@ -1,7 +1,15 @@
 function calculeScore() {
-  var score = document.getElementsByName("choix").values;
+  var score = 0;
+  var choix1 = document.getElementsByName("choix1");
+
   var choix = document.querySelectorAll('input[name="choix"]:checked');
   var nbchoix = choix.length;
+  for (let i = 0; i < nbchoix; i++) {
+    score += parseInt(choix1[i].nodeValue);
+  }
+  console.log(score);
+  console.log(choix1);
+  console.log(nbchoix);
   if (nbchoix > 3 || nbchoix < 3) {
     alert("choisir 3 choix !!");
   } else {
